2f92ca482e54d6de8fd8844db8a2e6755c6c0270
jimmy-recard <mxmlnstock@googlemail.com>
2015-08-30 18:08:36 +0200

    host url config

--- config/env/production.js
+++ config/env/production.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var NEO4J_URL = process.env.NEO4J_URL;
+var HOST_URL = process.env.HOST_URL;
 
 module.exports = {
 
   'environment': 'PRODUCTION',
-  'NEO4J_URL': NEO4J_URL
+  'NEO4J_URL': NEO4J_URL,
+  'HOST_URL': HOST_URL
 
 };
