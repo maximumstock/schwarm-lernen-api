79d20304760368425e083ff78d108c9fc2b01187
jimmy-recard <mxmlnstock@googlemail.com>
2015-09-24 01:01:04 +0200

    config fehlererkennung angepasst

--- config/env/production.js
+++ config/env/production.js
@@ -1,15 +1,22 @@
 'use strict';
 
 var NEO4J_URL = process.env.NEO4J_URL;
-var HOST_URL = process.env.HOST_URL || '';
 var API_SECRET = process.env.API_SECRET;
+var HOST_URL = process.env.HOST_URL || '';
 var DISABLE_AUTH = process.env.DISABLE_AUTH || false;
 var DEFAULT_USERNAME_LENGTH = process.env.DEFAULT_USERNAME_LENGTH || 7;
 var DEFAULT_PASSWORD_LENGTH = process.env.DEFAULT_PASSWORD_LENGTH || 7;
 
-if(!NEO4J_URL || !API_SECRET) {
-  throw new Error('Es fehlen Umgebungsvariablen');
+if(!NEO4J_URL) {
+  throw new Error('NEO4J_URL fehlt');
 }
+if(!API_SECRET) {
+  throw new Error('API_SECRET fehlt');
+}
+if(!NEO4J_URL) {
+  throw new Error('NEO4J_URL fehlt');
+}
+
 
 module.exports = {
 
